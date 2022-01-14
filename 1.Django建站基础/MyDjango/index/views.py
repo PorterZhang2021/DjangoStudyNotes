0b6cff40634923b124c848886c271da1bf3d70bb
PorterZhang <porterzhang@aliyun.com>
2022-01-14 13:40:22 +0800

    [Modified] views.py

--- 1.Django建站基础/MyDjango/index/views.py
+++ 1.Django建站基础/MyDjango/index/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html');
+    value = 'This is test!'
+    print(value)
+    return render(request, 'index.html')
