bb9f36827ffafad39a2283bbfc4d24e71254a41d
PorterZhang <porterzhang@aliyun.com>
2022-01-14 16:35:04 +0800

    [Update] 2.4.1 使用mysqlclient连接MySQL

--- MyDjango/MyDjango/settings.py
+++ MyDjango/MyDjango/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'MyDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
