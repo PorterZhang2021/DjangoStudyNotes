c8384e65a026949d7cb0108debcb121577501b85
PorterZhang <porterzhang@aliyun.com>
2022-01-23 09:26:54 +0800

    [Update] 3.1.3 路由变量的设置

--- MyDjango/index/urls.py
+++ MyDjango/index/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    # 添加带有字符类型、整型和slug的路由
+    path('<year>/<int:month>/<slug:day>', views.myVariable),
+    # 添加路由地址外的变量month
+    path('', views.index, {'month': '2019/10/10'}),
 ]
--- MyDjango/index/views.py
+++ MyDjango/index/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
-    value = 'This is test!'
-    print(value)
-    return render(request, 'index.html')
+def index(request, month):
+    return HttpResponse('这是路由地址之外的变量' + month)
+
+
+def myVariable(request, year, month, day):
+    return HttpResponse(str(year)+'/'+str(month)+'/'+str('day'))
