7260732ae230ea162df65e21c9df41f30bc80e16
PorterZhang <porterzhang@aliyun.com>
2022-01-24 18:40:24 +0800

    [Update] 4.1.1 返回响应内容-2

--- MyDjango/index/views.py
+++ MyDjango/index/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    html = '<h1>Hello World</h1>'
-    return HttpResponse(html, status=200)
+    value = {'title' : 'Hello MyDjango'}
+    return render(request, 'index.html', context=value)
